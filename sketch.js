b4bfc4e63b61aa8dcce958e23d1c1080f5421d46
Cibely Pereira <cibely.port@gmail.com>
2022-11-22 15:35:38 -0300

    inclusao das raquetes

--- sketch.js
+++ sketch.js
@@ -6,15 +6,37 @@ let raio = diametro / 2;
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
 
+let xRaquete = 5;
+let yRaquete = 150;
+let comprimentoRaquete = 10;
+let alturaRaquete = 90;
+
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velocidadeXOponenente;
+let velocidadeYOponente;
+
+let colidir = false;
+
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
 function setup() {
-  createCanvas(500, 300);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(50);
+  background(30);
   visualizarBolinha();
   movimentaBolinha();
   colisaoBordas();
+  visualizarRaquete(xRaquete, yRaquete);
+  movimentoMinhaRaquete();
+  movimentaRaqueteOponente();
+  colisaoMinhaRaquete(xRaquete, yRaquete);
+  colisaoRaqueteOponente(xRaqueteOponente, yRaqueteOponente);
+  incluiPlacar();
+  marcaPonto();
 }
 
 function visualizarBolinha() {
@@ -32,5 +54,54 @@ function colisaoBordas() {
   }
   if (yBolinha + raio > height || yBolinha - raio < 0) {
     velocidadeYBolinha *= -1;
+  }
+}
+
+function visualizarRaquete(x, y) {
+  rect(x, y, comprimentoRaquete, alturaRaquete);
+}
+
+function movimentoMinhaRaquete() {
+  if (keyIsDown(UP_ARROW)) {
+    yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yRaquete += 10;
+  }
+}
+
+function colisaoMinhaRaquete() {
+  if (xBolinha - raio < xRaquete + raqueteComprimento &&
+    yBolinha - raio < yRaquete + raqueteAltura &&
+    yBolinha + raio > yRaquete) {
+    velocidadeXBolinha *= -1;
+  }
+}
+
+function colisaoMinhaRaquete(x, y) {
+  colidir = collideRectCircle(x, y, comprimentoRaquete, alturaRaquete,
+    xBolinha, yBolinha, raio);
+  if (colidir) {
+    velocidadeXBolinha *= -1;
+  }
+}
+
+function colisaoRaqueteOponente() {
+  velocidadeYOponente = yBolinha - yRaqueteOponente - comprimentoRaquete / 2 - 30;
+  yRaqueteOponente += velocidadeYOponente
+}
+
+function incluiPlacar() {
+  fill(255);
+  text(meusPontos, 278, 26);
+  text(pontosDoOponente, 321, 26)
+}
+
+function marcaPonto() {
+  if (xBolinha > 590) {
+    meusPontos += 1;
+  }
+  if (xBolinha < 10) {
+    pontosDoOponente += 1;
   }
 }
