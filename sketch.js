f41d70e1844e914e2bc041e96c7d528118da4433
Cibely Pereira <cibely.port@gmail.com>
2022-11-21 22:51:38 -0300

    finalizando primeira aula - criação do jogo e bola pingpong

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+let raio = diametro / 2;
+
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 300);
 }
 
 function draw() {
-  background(220);
+  background(50);
+  visualizarBolinha();
+  movimentaBolinha();
+  colisaoBordas();
+}
+
+function visualizarBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function colisaoBordas() {
+  if (xBolinha + raio > width || xBolinha - raio < 0) {
+    velocidadeXBolinha *= -1;
+  }
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+    velocidadeYBolinha *= -1;
+  }
 }
